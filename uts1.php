9ebe57b6d0bd785796f1449c03fe1eaf6518ae4b
AzizLike29 <abdulfirdaus590@gmail.com>
2023-02-23 11:18:01 +0700

    Hasil pengerjaan tugas pemrograman web 1

---
+++ uts1.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Membuat Program UTS</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container">
+    <div class="row">
+    <div class="col-md-22 text-center"bgcolor="#CCCCCC">
+    <img src="pelitabangsa1.png" class="img-fluid" alt="pelitabangsa1.png" width="1300"height="200">
+ </div>
+ </div>
+    <div class="row">
+    <div class="col-md-12 text-center" style="background-color:#66CCCC;">
+    <a href="#">Home</a><a href="#" class="ml-3">E-Learning</a><a
+    href="#" class="ml-3">Prestasi</a><a href="#" class="ml-3">Pustaka</a>
+ </div>
+ </div>
+    <div class="row">
+    <div class="col-md-3" style="background-color:#99FFFF;">
+    <h1><center><button type="button" class="btn btn-outline-primary">Peserta UTS</button></h1></center>
+    <p><a>Nama          : Abdul Aziz Firdaus</a></p>
+    <p><a>Kelas         : TI.21.A.3</a></p>
+    <p><a>NIM           : 312110262</a></p>
+    <p><a>Mata Kuliah   : Pemrograman Web 1</a></p>
+    <div class="btn-group" role="group" aria-label="Basic mixed styles example">
+    <button type="button" class="btn btn-danger">Keluar Ujian</button>
+ </div>
+ </div>
+ <div class="col-md-9" style="background-color:#66CCFF;">
+    <center><h2><div class="btn-group" role="group" aria-label="Basic mixed styles example">
+    <button type="button" class="btn btn-success">KRS MAHASISWA</button>
+    </div><h2></center>
+    <table class="table table-bordered">
+<thead>
+    <tr>
+        <th scope="col"bgcolor="#CCFFFF">NO</th>
+        <th scope="col"bgcolor="#CCFFFF">MATA KULIAH</th>
+        <th scope="col"bgcolor="#CCFFFF">KODE</th>
+        <th scope="col"bgcolor="#CCFFFF">SKS</th>
+        <th scope="col"bgcolor="#CCFFFF">DOSEN</th>
+    </tr>
+ </thead>
+ <tbody>
+    <tr>
+        <th scope="row"bgcolor="#99FFCC">1</th>
+        <td>Pendidikan Agama 3</td>
+        <td>UPB301</td>
+        <td>1 SKS</td>
+        <td>Sally Badriya Hisniati, S.Pd., M.Pd.</td>
+    </tr>
+    <tr>
+        <th scope="row"bgcolor="#99FFCC">2</th>
+        <td>Probabilitas Statistika</td>
+        <td>TIF302</td>
+        <td>3 SKS</td>
+        <td>Ir. U.Darmanto Soer, M.Kom.</td>
+    </tr>
+    <tr>
+        <th scope="row" bgcolor="#99FFCC">3</th>
+        <td>Rekayasa Perangkat Lunak</td>
+        <td>TIF303</td>
+        <td>3 SKS</td>
+        <td>Drs. Muhtajuddin Danny, S.Kom., M.Kom.</td>
+    </tr>
+    <tr>
+        <th scope="row" bgcolor="#99FFCC">4</th>
+        <td>Pemrograman Orientasi Objek</td>
+        <td>TIF304</td>
+        <td>3 SKS</td>
+        <td>Najamuddin Dwi Miharja, S.Kom., M.Kom.</td>
+    </tr>
+    <tr>
+        <th scope="row" bgcolor="#99FFCC">5</th>
+        <td>Pemrograman Mobile 1</td>
+        <td>TIF305</td>
+        <td>4 SKS</td>
+        <td>Ismasari Nawangsih, M.Kom.</td>
+    </tr>
+    <tr>
+        <th scope="row" bgcolor="#99FFCC">6</th>
+        <td>Pemrograman Web 1</td>
+        <td>TIF306</td>
+        <td>3 SKS</td>
+        <td>Eko Budiarto, S.Kom., M.M.</td>
+    </tr>
+    <tr>
+        <th scope="row" bgcolor="#99FFCC">7</th>
+        <td>Jaringan Komputer</td>
+        <td>TIF307</td>
+        <td>3 SKS</td>
+        <td>Andri Firmansyah, S.Kom., M.Kom.</td>
+    </tr>
+    </tbody>
+ </table>
+ </div>
+ </div>
+    <div class="row" style="background-color:#3399FF;">
+    <div class="col-md-12 text-center">
+    <h7>Copyright © 2022 - UTS Pemrograman Web 1</h7>
+ </div>
+ </div>
+ </div>
+    <!-- jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+</body>
+</html>
